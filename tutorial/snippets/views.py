605c2dd117dd7d9f1f88c6426502c54956c2b4b3
Tordai János <23632757+ilgatto88@users.noreply.github.com>
2023-05-15 07:57:46 +0000

    DEV: tutorial finished

---
+++ tutorial/snippets/views.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides 'list', 'create', 'retrieve', 'update' and 'destroy' actions.
+    Additionally it also provides an extra 'highlight' action.
+    """
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """This viewset automatically provides 'list' and 'retrieve' actions."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
